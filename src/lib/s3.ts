10c44b133b8574a6e4ef05df5b525c0bf56ca656
Pushpaje Kumar <97593143+Pushpajekumar@users.noreply.github.com>
2023-12-18 13:17:10 +0530

    Update s3.ts
    removing credentials

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -5,8 +5,8 @@ export const s3 = new AWS.S3({
     accessKeyId: process.env.ACCESS_KEY_ID!,
     secretAccessKey: process.env.SECRET_ACCESS_KEY!,
   },
-  region: "us-east-1",
+  region: `${your_bucket_region}`,
   signatureVersion: "v4",
 });
 
-export const BUCKET_NAME = "icon-generator2130";
+export const BUCKET_NAME = `${your_bucket_name}`;
